cdbd0916f01bf4dd5e39ef8ff7f98ad0e842333b
Maxim93mk <kinjashov.m@rambler.ru>
2024-04-27 17:41:17 +0300

    Страница корзины, footer - готово

--- src/pages/cart-page/cart-page.jsx
+++ src/pages/cart-page/cart-page.jsx
@@ -10,6 +10,10 @@ function CartPage() {
                 <button className='cart-header__back-btn'></button>
                 <h1 className='cart-header__title'>КОРЗИНА С ВЫБРАННЫМИ ТОВАРАМИ</h1>
             </header>
+            <footer className='cart-footer'>
+                <p className='cart-footer_descr-summ'>ЗАКАЗ НА СУММУ: <span className='cart-footer_summ'>XXX</span></p>
+                <button className='cart-footer__order-btn'>Оформить заказ</button>
+            </footer>
         </>
     );
 }
